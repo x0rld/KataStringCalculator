46867bc7addddda2841829cae5cac03369f528f2
x0rld <git@x0rld.eu>
2022-09-21 15:51:31 +0200

    parameterize test

--- StringCalculator/StringCalculate.cs
+++ StringCalculator/StringCalculate.cs
@@ -1,22 +1,17 @@
-﻿namespace StringCalculator;
+﻿using System.Linq;
+
+namespace StringCalculator;
 
 public class StringCalculate
 {
     public static int Add(string input)
     {
-        if (input == "3,4")
-        {
-            return 3+4;
-        }
-        if (input == "1,2")
-        {
-            return 1+2;
-        }
         if (IsBlank(input))
         {
             return 0;
         }
-        return int.Parse(input);
+        
+        return input.Split(",").Sum(int.Parse);
     }
 
     private static bool IsBlank(string input)
--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -7,27 +7,22 @@ public class StringCalculateTest
 {
     [Theory]
     [InlineData("4",4)]
-    public void given_number_string_returns_number(string input, int expected)
+    public void given_one_number_string_returns_number(string input, int expected)
     {
         var actual = StringCalculate.Add(input);
         
         Check.That(actual).Equals(expected);
     }
-
-    [Fact]
-    public void given_two_numbers_string_returns_sum_of_numbers()
+    
+    [Theory]
+    [InlineData("1,2",3)]
+    [InlineData("3,4",7)]
+    [InlineData("3,4,3",10)]
+    public void any_numbers_string_returns_sum_of_numbers(string input, int expected)
     {
-        var actual = StringCalculate.Add("1,2");
+        var actual = StringCalculate.Add(input);
 
-        Check.That(actual).Equals(3);
-    }
-
-    [Fact]
-    public void todo()
-    {
-        var actual = StringCalculate.Add("3,4");
-
-        Check.That(actual).Equals(7);
+        Check.That(actual).Equals(expected);
     }
 
 }

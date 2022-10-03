28022fea1c860e5da1290c3501247238586154c9
x0rld <git@x0rld.eu>
2022-10-03 20:33:41 +0200

    change format

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -5,19 +5,17 @@ namespace StringCalculator;
 
 public class StringCalculateTest
 {
-    [Theory]
-    [InlineData("4",4)]
-    public void given_one_number_string_returns_number(string input, int expected)
+    [Fact]
+    public void given_one_number_string_returns_number()
     {
-        var actual = StringCalculate.Add(input);
-        
-        Check.That(actual).Equals(expected);
+        var actual = StringCalculate.Add("4");
+        Check.That(actual).Equals(4);
     }
-    
+
     [Theory]
-    [InlineData("1,2",3)]
-    [InlineData("3,4",7)]
-    [InlineData("3,4,3",10)]
+    [InlineData("1,2", 3)]
+    [InlineData("3,4", 7)]
+    [InlineData("3,4,3", 10)]
     public void any_numbers_string_returns_sum_of_numbers(string input, int expected)
     {
         var actual = StringCalculate.Add(input);
@@ -29,7 +27,7 @@ public class StringCalculateTest
     [InlineData("3\n4", 7)]
     [InlineData("3,4\n3", 10)]
     public void any_number_either_comma_or_newline_separator_returns_sum_of_numbers(string input, int expected)
-    {        
+    {
         var actual = StringCalculate.Add(input);
         Check.That(actual).Equals(expected);
     }
